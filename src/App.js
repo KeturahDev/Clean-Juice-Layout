4b38ddec1a75d60b8900cacac016167622bee0e5
Keturah Howard <keturah.dev@gmail.com>
2021-04-25 22:15:55 -0700

    update template

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './styles/App.css';
-import Header from './components/Header'
-import Main from './components/Main';
+import Header from './components/Header.jsx'
+import Main from './components/Main.jsx';
 
 function App() {
   return (
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import styled from 'styled-components'
 
 const Box = styled.div`
-  diplay: flex;
+  display: flex;
   margin: 12%;
   padding: 5%;
   border: 2px solid orange;
+  flex-direction: column;
+  align-items:center;
+  justify-content: center;
 `
 const Main = () => {
   return (
     <React.Fragment>
       <Box>
+        <div>
         Main content's components.
+        </div>
       </Box>
     </React.Fragment>
   )
