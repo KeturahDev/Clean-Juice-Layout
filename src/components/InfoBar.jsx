5a31f9e4c788eb2fa7d13146d57acddfe72ecce5
Keturah Howard <keturah.dev@gmail.com>
2021-05-08 20:17:41 -0700

    successfully render local image

--- src/components/InfoBar.jsx
+++ src/components/InfoBar.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import OrangBottle from './OrangeBottle.jpg';
 
 const Box = styled.div`
   margin: auto;
 `;
 
 const InfoBar = () => {
-  const [selected, setSelected] = useState('OrangeBottle.png');
+  const [selected, setSelected] = useState(OrangBottle);
 
   return (
     <Box>
       <button onClick={() => alert('back')}>back</button>
-      <img src="OrangeBottle.jpg" alt="img of bottle" />
-      {/* <img src={require(OrangeBottle.jpg)} alt="img of bottle" /> */}
+      <img src={selected} alt="img of bottle" />
       <button onClick={() => alert('forward')}>forward</button>
     </Box>
   );
